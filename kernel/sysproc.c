4eca60d567f1e31f8384087f98bc22e7deb2d417
amn3m <a.mohamed2230@nu.edu.eg>
2025-05-24 03:14:53 +0300

    Task 6 (Alarm): Implement sys_alarm and add placeholder alarmtest user program

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -91,3 +91,34 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_alarm(void)
+{
+  int seconds;
+  uint64 handler_addr;
+  struct proc *p = myproc();
+  int prev_alarm_remaining_seconds = 0;
+
+  argint(0, &seconds);
+  argaddr(1, &handler_addr);
+  acquire(&p->lock);
+
+  if (p->alarm_interval > 0 && p->alarm_ticks_left > 0) {
+    prev_alarm_remaining_seconds = (p->alarm_ticks_left + HZ - 1) / HZ;
+  }
+
+  if (seconds <= 0) {
+    p->alarm_interval = 0;
+    p->alarm_ticks_left = 0;
+    p->alarm_handler_addr = 0;
+  } else {
+    p->alarm_interval = seconds * HZ;
+    p->alarm_ticks_left = p->alarm_interval;
+    p->alarm_handler_addr = handler_addr;
+  }
+
+  release(&p->lock);
+
+  return prev_alarm_remaining_seconds;
+}
---
+++ user/alarmtest.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  printf("Alarm test program placeholder\n");
+  exit(0);
+}
